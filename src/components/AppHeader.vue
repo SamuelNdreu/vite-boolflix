a95f3629994f99f978c15b99d8cd4d68c1d18151
SamuelNdreu <samwellsoficcial@gmail.com>
2023-01-16 12:52:36 +0100

    milestone 1 almost done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,37 @@
 <script>
+import axios from 'axios';
 import { store } from '../store.js'
 export default {
     name: 'AppHeader',
     data () {
         return {
             store,
+            apiKey:'56fe84a4653ee995f78bcfae8038ddd7',
+            apiUri:'https://api.themoviedb.org/3/search/movie',
         }
     },
     methods: {
+        getMovies(){
+            //chiamata API
+            axios.get(this.apiUri,{ 
+                params:{
+                    api_key:this.apiKey,
+                    query: 'Furios'
+                }
+                
+            })
+            .then( (response) => {
+                console.log(response.data.results);
+                this.store.moviesList = response.data.results;
+            })
+            .catch( function (error) {
+                console.warn (error)
+            });
 
+        }
     },
     created() {
-        
+        this.getMovies();
     },
 }
 </script>
@@ -26,6 +46,14 @@ export default {
                         aria-describedby="button-search" v-model="store.searchText" @keyup.enter="$emit('searchMovie')">
                     <button class="btn btn-outline-secondary" type="button" id="button-search"
                         @click="$emit('searchMovie')">Cerca</button>
+                    <ul>
+                        <li v-for="movieElement in store.moviesList"></li>
+                        <h3>{{ movieElement.title }}</h3>
+                        <h5>{{ movieElement.original_Title }}</h5>
+                        <p>Language: {{ movieElement.original_language }} <br>
+                            Rating: {{ vote_average }} stars
+                        </p>
+                    </ul>    
     
                 </div>
             </div>
@@ -33,5 +61,8 @@ export default {
     </section>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+    button{
+        margin-left: 1rem;
+    }
 </style>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
     searchText: '',
-    searchedMovie: [],
+    moviesList: [],
     searchedTvshow: [],
 
 })
